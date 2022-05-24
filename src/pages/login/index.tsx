6b824c1cfb88635ede7636d829a8774f6a1407dd
Diego Lincoln <diego.lincoln@hotmail.com>
2022-05-24 01:49:39 -0300

    chore: add linebreak between elements

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,6 +5,7 @@ function Login() {
 		<>
 			<form data-testid='form-element'>
 				<input data-testid='name-input' type='text' placeholder='Nome' />
+
 				<input data-testid='password-input' type='password' placeholder='Senha' />
 			</form>
 
